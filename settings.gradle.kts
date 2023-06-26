3f419653a105414fc68f0602c0d32c5adf84bbaa
邹铭杰 <zoumingjie@corp.netease.com>
2023-06-26 17:34:20 +0800

    feat:Add Github Maven Repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,25 @@ pluginManagement {
     includeBuild("kmmdeploy")
 }
 
+fun getProperty(propertyName: String, defaultValue: String?): String? {
+    val properties = java.util.Properties()
+    val inputStream = File(rootDir, "local.properties").inputStream()
+    properties.load(inputStream)
+    return properties.getProperty(propertyName, defaultValue)
+}
+
 dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
 //        maven("repos/releases")
+//        maven {
+//            url = uri("https://maven.pkg.github.com/KevinnZou/KMMDeployPlugin")
+//            credentials {
+//                username = getProperty("gpr.user","")
+//                password = getProperty("gpr.key","")
+//            }
+//        }
     }
 }
 
